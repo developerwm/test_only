c10424e0bee9609032d3dd63aabed19defbbed28
Taua <root@Tauas-MacBook-Pro.local>
2020-06-21 14:36:55 -0300

    first commit

---
+++ android/app/src/main/kotlin/app/test/flutter_app_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.test.flutter_app_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/profile_user.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  MapScreenState createState() => MapScreenState();
+}
+
+class MapScreenState extends State<ProfilePage>
+    with SingleTickerProviderStateMixin {
+  bool _status = true;
+  final FocusNode myFocusNode = FocusNode();
+  final _emailIdController = new TextEditingController();
+  final _addressController = new TextEditingController();
+  final _nameController = new TextEditingController();
+  var idDoc = "1";
+
+  _getUpdateprofile() async {
+    try {
+      Firestore.instance.collection('user').document(idDoc).updateData(
+          {'name': _nameController.text, 'address': _addressController.text});
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  _getProfile() {
+    Firestore.instance
+        .collection('user')
+        .where("email", isEqualTo: _emailIdController.text)
+        .snapshots()
+        .listen(
+          (data) => data.documents.forEach(
+            (doc) {
+              idDoc = doc.documentID;
+              _emailIdController.text = doc["email"];
+              _addressController.text = doc["address"];
+              _nameController.text = doc["name"];
+            },
+          ),
+        );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(
+        color: Colors.white,
+        child: new ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                new Container(
+                  color: Color(0xffFFFFFF),
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 25.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        TextFormField(
+                          controller: _emailIdController,
+                          decoration: const InputDecoration(
+                            hintText: 'Email',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _nameController,
+                          decoration: const InputDecoration(
+                            hintText: 'Name',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _addressController,
+                          decoration: const InputDecoration(
+                            hintText: 'Address',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  child: FlatButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.all(8.0),
+                    splashColor: Colors.blueAccent,
+                    onPressed: () {
+                      _getUpdateprofile();
+                    },
+                    child: Text(
+                      "save",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: FlatButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.all(8.0),
+                    splashColor: Colors.blueAccent,
+                    onPressed: () {
+                      print("logdev");
+                      _getProfile();
+                    },
+                    child: Text(
+                      "get profile",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
+  Widget _getActionButtons() {
+    return Padding(
+      padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 45.0),
+      child: new Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 10.0),
+              child: Container(
+                  child: new RaisedButton(
+                child: new Text("Save"),
+                textColor: Colors.white,
+                color: Colors.green,
+                onPressed: () {
+                  setState(() {
+                    _status = true;
+                    FocusScope.of(context).requestFocus(new FocusNode());
+                  });
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
+              )),
+            ),
+            flex: 2,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getEditIcon() {
+    return new GestureDetector(
+      child: new CircleAvatar(
+        backgroundColor: Colors.red,
+        radius: 14.0,
+        child: new Icon(
+          Icons.edit,
+          color: Colors.white,
+          size: 16.0,
+        ),
+      ),
+      onTap: () {
+        setState(() {
+          _status = false;
+        });
+      },
+    );
+  }
+}
